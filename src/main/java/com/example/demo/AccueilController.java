5b406e7d12b975f8233774f035ce66bd60bd8de2
ClemAMolette <clementinegaga79@outlook.fr>
2023-10-18 16:17:45 +0200

    début d'accueil et navigation

---
+++ src/main/java/com/example/demo/AccueilController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class AccueilController {
+
+    @GetMapping("/accueil")
+    public String showAccueil(Model model) {
+        return "accueil";
+    }
+
+}
