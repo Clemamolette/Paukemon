78e1d3a12e271b8e56d64072726db4dad857cd84
ClemAMolette <clementinegaga79@outlook.fr>
2023-10-18 15:07:43 +0200

    Initialisation

---
+++ src/main/java/com/example/demo/Type.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Type {
+
+    @Id @GeneratedValue
+    private Long id;
+    @Column
+    private String nom;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public String toString() {
+        return nom;
+    }
+}
