e1ccb10ee0acab520603087f18a4d3494e2eab8d
ClemAMolette <clementinegaga79@outlook.fr>
2023-10-18 17:29:52 +0200

    ajout écran de chargement + début css + lien pages navigation

---
+++ src/main/java/com/example/demo/BoostersController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoostersController {
+
+    @GetMapping("/boosters")
+    public String showBoosters(Model model) {
+        return "boosters";
+    }
+
+}
