ad2ca00a98fb0e6337b2611e606ca8a01e737f20
ClemAMolette <clementinegaga79@outlook.fr>
2023-10-21 18:18:19 +0200

    formatage de la réponse API

--- src/main/java/com/example/demo/CartesController.java
+++ src/main/java/com/example/demo/CartesController.java
@@ -1,10 +1,16 @@
 package com.example.demo;
 
+import java.util.*;
+
 import com.mashape.unirest.http.JsonNode;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
 
 @Controller
 public class CartesController {
@@ -15,13 +21,10 @@ public class CartesController {
     @GetMapping("/mescartes")
     public String showCartes(Model model) {
 
-        JsonNode pokemonCardsData = null;
+        ArrayList<ArrayList<String>> pokemonCardsData = null;
         pokemonCardsData = pokemonService.getCards();
 
         model.addAttribute("pokemonCards", pokemonCardsData);
-
-        System.out.println(pokemonCardsData);
-
 
         return "mescartes";
     }
--- src/main/java/com/example/demo/PokemonService.java
+++ src/main/java/com/example/demo/PokemonService.java
@@ -2,25 +2,67 @@ package com.example.demo;
 
 import com.mashape.unirest.http.*;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class PokemonService {
 
-    private String apiURL = "https://api.pokemontcg.io/v2/cards?select=id,name,hp,types,nationalPokedexNumbers,images";
-    private String apiKEY = "e030d28b-a210-4552-bec5-3b63be6b970b";
+    private String apiURL1 = "https://api.pokemontcg.io/v2/cards?select=id,name,hp,types,nationalPokedexNumbers,images,rarity&q=set.id:bw1";
+    private String apiURL2 = "https://api.pokemontcg.io/v2/cards?select=id,name,hp,types,nationalPokedexNumbers,images,rarity&q=set.id:bw2";
+    private String apiKEY = "";
 
-    public JsonNode getCards() {
+    public ArrayList<ArrayList<String>> getCards() {
         HttpResponse<JsonNode> jsonResponse;
         {
+            ArrayList<ArrayList<String>> data;
             try {
-                jsonResponse = Unirest.get(apiURL)
+                // première requête avec une extension de carte
+                jsonResponse = Unirest.get(apiURL1)
                         .header("X-Api-Key", apiKEY)
                         .asJson();
+
+                // création des tableaux pour contenir les cartes rares et communes
+                data = new ArrayList<>();
+                ArrayList<String> communes = new ArrayList<String>();
+                ArrayList<String> rares = new ArrayList<String>();
+
+
+                //récupération des données de la réponse et classement dans les deux tableaux selon la rareté
+                JSONArray data_json = (JSONArray) jsonResponse.getBody().getObject().get("data");
+                int len = data_json.length();
+                dispatchRarity(data_json, communes, rares, len);
+
+                // Deuxième requête avec une autre extension de cartes
+                jsonResponse = Unirest.get(apiURL1)
+                        .header("X-Api-Key", apiKEY)
+                        .asJson();
+                data_json = (JSONArray) jsonResponse.getBody().getObject().get("data");
+                len = data_json.length();
+                dispatchRarity(data_json, communes, rares, len);
+
+                // on regroupe les deux listes dans une même autre liste
+                data.add(communes);
+                data.add(rares);
+
             } catch (UnirestException e) {
                 throw new RuntimeException(e);
             }
-            return jsonResponse.getBody();
+            return data;
+        }
+    }
+
+    private void dispatchRarity(JSONArray data_json, ArrayList<String> communes, ArrayList<String> rares, int len) {
+        for (int i=0;i<len;i++){
+            JSONObject card = (JSONObject) data_json.get(i);
+            if (card.get("rarity").toString().contains("ommon")) {
+                communes.add(card.toString());
+            } else {
+                rares.add(card.toString());
+            }
         }
     }
 }
