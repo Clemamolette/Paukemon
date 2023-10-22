5e6e3b976b1c8353ed7daceaa0b9ed0e5ecd6af6
Clémentine <93268012+Clemamolette@users.noreply.github.com>
2023-10-22 23:07:08 +0200

    Delete src/main/resources/static/cartes.js

--- src/main/resources/static/cartes.js
+++
@@ -1,7 +0,0 @@
-import pokemon from 'pokemontcgsdk';
-
-pokemon.configure({apiKey: 'e030d28b-a210-4552-bec5-3b63be6b970b'});
-pokemon.card.find('base1-4')
-    .then(card => {
-        console.log(card.name) // "Charizard"
-    });
