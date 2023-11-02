ec7ca834a956ef22a2ae97d3d25329a6874d5462
ClemAMolette <clementinegaga79@outlook.fr>
2023-11-02 23:02:52 +0100

    Page d'accueil faite + correction de bugs dans l'ajout de cartes

--- src/main/java/com/example/demo/Controller/addCarteController.java
+++ src/main/java/com/example/demo/Controller/addCarteController.java
@@ -7,6 +7,7 @@ import com.example.demo.Model.Type;
 import com.example.demo.Repository.MesCartesRepository;
 import com.example.demo.Repository.TypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,22 +32,30 @@ public class addCarteController {
     @PostMapping("/addCarte")
     public String addCarte(@ModelAttribute Carte carte, Model model) {
 
-        // vérifier si tous les champs sont remplis
-        if (areFieldsEmpty(carte)) {
-            model.addAttribute("failMessage", "Veuillez remplir tous les champs.");
+        try {
             List<Type> types = typeRepo.findAll();
-            model.addAttribute("types", types);
+            // vérifier si tous les champs sont remplis
+            if (areFieldsEmpty(carte)) {
+                model.addAttribute("failMessage", "Veuillez remplir tous les champs.");
+                model.addAttribute("types", types);
+                return "addCarte";
+            }
+
+            int next_size = mesCartesRepo.findAll().size() + 1;
+            String id = carte.getSerie() + next_size;
+            carte.setId(id);
+
+            mesCartesRepo.save(carte);
+            model.addAttribute("types",types);
+            model.addAttribute("successMessage", "La carte a été ajoutée avec succès !");
+
+            return "addCarte";
+        } catch (DataIntegrityViolationException e) {
+            List<Type> types = typeRepo.findAll();
+            model.addAttribute("types",types);
+            model.addAttribute("failMessage", "Chaînes de caractères trop longues.");
             return "addCarte";
         }
-
-        int next_size = mesCartesRepo.findAll().size() + 1;
-        String id = carte.getSerie() + next_size;
-        carte.setId(id);
-
-        mesCartesRepo.save(carte);
-        model.addAttribute("successMessage", "La carte a été ajoutée avec succès !");
-
-        return "addCarte";
     }
 
     private boolean areFieldsEmpty(Carte carte) {
